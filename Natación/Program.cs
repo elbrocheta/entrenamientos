ed050edbdca6063d8e6be87dff9bf74d5ec21a95
David <dcarpioc@gmail.com>
2018-09-19 15:30:37 +0200

    Cambio de nombre del formulario principal

--- Natación/Program.cs
+++ Natación/Program.cs
@@ -16,7 +16,7 @@ namespace Natación
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
